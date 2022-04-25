7b6a4a5269769f6406c7e384f6edc0b182b1b604
rael guimaraes <raelguimaraes8@hotmail.com>
2022-04-25 13:55:55 +0100

    connecting frontend to backend

--- appetizing/src/app/dashboard-add-admin/dashboard-add-admin.component.ts
+++ appetizing/src/app/dashboard-add-admin/dashboard-add-admin.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { FormGroup, FormControl, Validators, FormBuilder, AbstractControl, ValidatorFn } from '@angular/forms';
+import { first, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dashboard-add-admin',
@@ -8,14 +11,70 @@ import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Rout
 })
 export class DashboardAddAdminComponent implements OnInit {
 
-  ngOnInit(): void {
-  }
-  constructor(private router: Router) {
+  loginForm: any;
+  clicked = false;
+  errorSign = false;
+
+  constructor(private router: Router, private http: HttpClient, private formBuilder: FormBuilder) {
   }
 
-  dashboardAdmin(){
+  ngOnInit(): void {
+
+    this.loginForm = new FormGroup({
+      fullName: new FormControl(undefined, [Validators.required]),
+      username: new FormControl(undefined, [Validators.required]),
+      password: new FormControl([undefined, [Validators.required, Validators.minLength(6), Validators.minLength(60)]])
+    })
+  }
+
+  httpPost(url: string, request: any) {
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      })
+    };
+
+    return this.http.post<any>(url, request, httpOptions).pipe(map((data) => {
+      return data;
+    }));
+  }
+
+  registerAdmin() {
+    if (this.loginForm.valid) {
+
+      this.clicked = true;
+
+      let data = {
+        fullName: this.loginForm.controls['fullName'].value,
+        username: this.loginForm.controls['username'].value,
+        password: this.loginForm.controls['password'].value
+      };
+
+      alert('next alert will be showing the json struct');
+      alert(JSON.stringify(data));
+
+      this.httpPost("https://backend/", data).pipe(first())
+        .subscribe(
+          data => {
+            this.errorSign = false;
+          },
+          error => {
+            this.errorSign = true;
+            this.clicked = false;
+          });
+    }
+    else {
+      alert("Fill the fields!");
+    }
+  }
+
+
+  dashboardAdmin() {
     this.router.navigate(['dashboard-admin']);
 
   }
-  
+
+
 }
